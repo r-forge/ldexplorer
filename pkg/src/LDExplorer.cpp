2c858fba841e48af53de3b1ddb8e5ce11a7dcd07
dtaliun (Daniel Taliun) <daniel.taliun@eurac.edu>
2013-01-25 22:11:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ldexplorer@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/LDExplorer.cpp
+++ pkg/src/LDExplorer.cpp
@@ -26,6 +26,119 @@ using namespace std;
 
 extern "C" {
 
+	const char* validateString(SEXP value, const char* name, bool optional) {
+		if (isNull(value)) {
+			if (!optional) {
+				error("%s is NULL.", name);
+			} else {
+				return NULL;
+			}
+		}
+
+		if (!isString(value)) {
+			error("%s is not a string.", name);
+		}
+
+		if (length(value) <= 0) {
+			error("%s contains no values.", name);
+		}
+
+		if (length(value) > 1) {
+			error("%s contains multiple values.", name);
+		}
+
+		if (STRING_ELT(value, 0) == R_NaString) {
+			error("%s is NA.", name);
+		}
+
+		if (STRING_ELT(value, 0) == R_BlankString) {
+			error("%s is blank.", name);
+		}
+
+		return CHAR(STRING_ELT(value, 0));
+	}
+
+	double validateDouble(SEXP value, const char* name, bool optional) {
+		double c_value = numeric_limits<double>::quiet_NaN();
+
+		if (isNull(value)) {
+			if (!optional) {
+				error("%s is NULL.", name);
+			} else {
+				return numeric_limits<double>::quiet_NaN();;
+			}
+		}
+
+		if (!isNumeric(value)) {
+			error("%s is not numeric.", name);
+		}
+
+		if (isLogical(value)) {
+			error("%s is logical.", name);
+		}
+
+		if (length(value) <= 0) {
+			error("%s contains no values.", name);
+		}
+
+		if (length(value) > 1) {
+			error("%s contains multiple values.", name);
+		}
+
+		if (isInteger(value)) {
+			c_value = (double)INTEGER(value)[0];
+		} else {
+			c_value = REAL(value)[0];
+			if (isnan(c_value)) {
+				error("%s is NA/NaN.", name);
+			}
+		}
+
+		return c_value;
+	}
+
+	long int validateInteger(SEXP value, const char* name, bool optional) {
+		double c_value_double = numeric_limits<double>::quiet_NaN();
+		long int c_value_int = numeric_limits<long int>::min();
+
+		if (isNull(value)) {
+			if (!optional) {
+				error("%s is NULL.", name);
+			} else {
+				return numeric_limits<long int>::min();
+			}
+		}
+
+		if (!isNumeric(value)) {
+			error("%s is not numeric.", name);
+		}
+
+		if (isLogical(value)) {
+			error("%s is logical.", name);
+		}
+
+		if (length(value) <= 0) {
+			error("%s contains no values.", name);
+		}
+
+		if (length(value) > 1) {
+			error("%s contains multiple values.", name);
+		}
+
+		if (isInteger(value)) {
+			c_value_int = INTEGER(value)[0];
+		} else {
+			c_value_double = REAL(value)[0];
+			if (isnan(c_value_double)) {
+				error("%s is NA/NaN.", name);
+			}
+			c_value_int = (long int)c_value_double;
+		}
+
+		return c_value_int;
+	}
+
+
 	SEXP mig(SEXP phase_file, SEXP output_file, SEXP file_format, SEXP legend_file,
 			SEXP region, SEXP maf, SEXP ci_method, SEXP ci_precision, SEXP ld_ci, SEXP ehr_ci, SEXP ld_fraction,
 			SEXP pruning_method, SEXP window) {
@@ -38,120 +151,24 @@ extern "C" {
 		long int c_region_int[2] = {0, numeric_limits<long int>::max()};
 		double c_maf = 0.0;
 		const char* c_ci_method = NULL;
-		double c_ci_precision_double = 0.0;
-		long int c_ci_precision_int = 0;
+		long int c_ci_precision = 0;
 		double c_ld_ci[] = {0.0, 0.0};
 		double c_ehr_ci = 0.0;
 		double c_ld_fraction = 0.0;
 		const char* c_pruning_method = NULL;
-		double c_window_double = 0.0;
-		long int c_window_int = 0;
+		long int c_window = 0;
 
 //		Validate phase_file argument
-		if (isNull(phase_file)) {
-			cout << "phase_file is null" << endl;
-		}
-
-		if (!isString(phase_file)) {
-			cout << "phase_file is not string" << endl;
-		}
-
-		if (length(phase_file) <= 0) {
-			cout << "phase_file is empty" << endl;
-		}
-
-		if (length(phase_file) > 1) {
-			cout << "phase_file has multiple values." << endl;
-		}
-
-		if (STRING_ELT(phase_file, 0) == R_NaString) {
-			cout << "phase_file is na" << endl;
-		}
-
-		if (STRING_ELT(phase_file, 0) == R_BlankString) {
-			cout << "phase_file is blank" << endl;
-		}
-
-		c_phase_file = CHAR(STRING_ELT(phase_file, 0));
+		c_phase_file = validateString(phase_file, "phase_file", false);
 
 //		Validate output_file argument
-		if (isNull(output_file)) {
-			cout << "output_file is null" << endl;
-		}
-
-		if (!isString(output_file)) {
-			cout << "output_file is not string" << endl;
-		}
-
-		if (length(output_file) <= 0) {
-			cout << "output_file is empty" << endl;
-		}
-
-		if (length(output_file) > 1) {
-			cout << "output_file has multiple values." << endl;
-		}
-
-		if (STRING_ELT(output_file, 0) == R_NaString) {
-			cout << "output_file is na" << endl;
-		}
-
-		if (STRING_ELT(output_file, 0) == R_BlankString) {
-			cout << "output_file is blank" << endl;
-		}
-
-		c_output_file = CHAR(STRING_ELT(output_file, 0));
+		c_output_file = validateString(output_file, "output_file", false);
 
 //		Validate file_format argument
-		if (isNull(file_format)) {
-			cout << "file_format is null" << endl;
-		}
-
-		if (!isString(file_format)) {
-			cout << "file_format is not string" << endl;
-		}
-
-		if (length(file_format) <= 0) {
-			cout << "file_format is empty" << endl;
-		}
-
-		if (length(file_format) > 1) {
-			cout << "file_format has multiple values." << endl;
-		}
-
-		if (STRING_ELT(file_format, 0) == R_NaString) {
-			cout << "file_format is na" << endl;
-		}
-
-		if (STRING_ELT(file_format, 0) == R_BlankString) {
-			cout << "file_format is blank" << endl;
-		}
-
-		c_file_format = CHAR(STRING_ELT(file_format, 0));
+		c_file_format = validateString(file_format, "file_format", false);
 
 //		Validate legend_file argument
-		if (!isNull(legend_file)) {
-			if (!isString(legend_file)) {
-				cout << "legend_file is not string" << endl;
-			}
-
-			if (length(legend_file) <= 0) {
-				cout << "legend_file is empty" << endl;
-			}
-
-			if (length(legend_file) > 1) {
-				cout << "legend_file has multiple values." << endl;
-			}
-
-			if (STRING_ELT(legend_file, 0) == R_NaString) {
-				cout << "legend_file is na" << endl;
-			}
-
-			if (STRING_ELT(legend_file, 0) == R_BlankString) {
-				cout << "legend_file is blank" << endl;
-			}
-
-			c_legend_file = CHAR(STRING_ELT(legend_file, 0));
-		}
+		c_legend_file = validateString(legend_file, "legend_file", true);
 
 //		Validate region argument
 		if (!isNull(region)) {
@@ -198,98 +215,17 @@ extern "C" {
 		}
 
 //		Validate maf argument
-		if (isNull(maf)) {
-			cout << "maf is null" << endl;
-		}
-
-		if (!isNumeric(maf)) {
-			cout << "maf is not numeric" << endl;
-		}
-
-		if (isLogical(maf)) {
-			cout << "maf is logical" << endl;
-		}
-
-		if (length(maf) <= 0) {
-			cout << "maf is empty" << endl;
-		}
-
-		if (length(maf) > 1) {
-			cout << "maf has multiple values" << endl;
-		}
-
-		if (isInteger(maf)) {
-			c_maf = (double)INTEGER(maf)[0];
-		} else {
-			c_maf = REAL(maf)[0];
-			if (isnan(c_maf)) {
-				cout << "c_maf is not a number" << endl;
-			}
-		}
-
+		c_maf = validateDouble(maf, "maf", false);
 		if ((c_maf < 0.0) || (c_maf > 0.5)) {
 			cout << "c_maf must be in [0, 0.5]" << endl;
 		}
 
 //		Validate ci_method argument
-		if (isNull(ci_method)) {
-			cout << "ci_method is null" << endl;
-		}
-
-		if (!isString(ci_method)) {
-			cout << "ci_method is not string" << endl;
-		}
-
-		if (length(ci_method) <= 0) {
-			cout << "ci_method is empty" << endl;
-		}
-
-		if (length(ci_method) > 1) {
-			cout << "ci_method has multiple values." << endl;
-		}
-
-		if (STRING_ELT(ci_method, 0) == R_NaString) {
-			cout << "ci_method is na" << endl;
-		}
-
-		if (STRING_ELT(ci_method, 0) == R_BlankString) {
-			cout << "ci_method is blank" << endl;
-		}
-
-		c_ci_method = CHAR(STRING_ELT(ci_method, 0));
+		c_ci_method = validateString(ci_method, "ci_method", false);
 
 //		Validate ci_precision argument
-		if (isNull(ci_precision)) {
-			cout << "ci_precision is null" << endl;
-		}
-
-		if (!isNumeric(ci_precision)) {
-			cout << "ci_precision is not numeric" << endl;
-		}
-
-		if (isLogical(ci_precision)) {
-			cout << "ci_precision is logical" << endl;
-		}
-
-		if (length(ci_precision) <= 0) {
-			cout << "ci_precision is empty" << endl;
-		}
-
-		if (length(ci_precision) > 1) {
-			cout << "ci_precision has multiple values." << endl;
-		}
-
-		if (isInteger(ci_precision)) {
-			c_ci_precision_int = INTEGER(ci_precision)[0];
-		} else {
-			c_ci_precision_double = REAL(ci_precision)[0];
-			if (isnan(c_ci_precision_double)) {
-				cout << "ci_precision is nan" << endl;
-			}
-			c_ci_precision_int = (long int)c_ci_precision_double;
-		}
-
-		if (c_ci_precision_int <= 0) {
+		c_ci_precision = validateInteger(ci_precision, "ci_precision", false);
+		if (c_ci_precision <= 0) {
 			cout << "ci_precision must be greater than 0" << endl;
 		}
 
@@ -337,135 +273,25 @@ extern "C" {
 		}
 
 //		Validate ehr_ci argument
-		if (isNull(ehr_ci)) {
-			cout << "ehr_ci is null" << endl;
-		}
-
-		if (!isNumeric(ehr_ci)) {
-			cout << "ehr_ci is not numeric" << endl;
-		}
-
-		if (isLogical(ehr_ci)) {
-			cout << "ehr_ci is logical" << endl;
-		}
-
-		if (length(ehr_ci) <= 0) {
-			cout << "ehr_ci is empty" << endl;
-		}
-
-		if (length(ehr_ci) > 1) {
-			cout << "ehr_ci has multiple values" << endl;
-		}
-
-		if (isInteger(ehr_ci)) {
-			c_ehr_ci = (double)INTEGER(ehr_ci)[0];
-		} else {
-			c_ehr_ci = REAL(ehr_ci)[0];
-			if (isnan(c_ehr_ci)) {
-				cout << "c_ehr_ci is not a number" << endl;
-			}
-		}
-
+		c_ehr_ci = validateDouble(ehr_ci, "ehr_ci", false);
 		if ((c_ehr_ci < 0.0) || (c_ehr_ci > 1.0)) {
 			cout << "c_ehr_ci must be in [0.0, 1.0]" << endl;
 		}
 
 //		Validate ld_fraction argument
-		if (isNull(ld_fraction)) {
-			cout << "ld_fraction is null" << endl;
-		}
-
-		if (!isNumeric(ld_fraction)) {
-			cout << "ld_fraction is not numeric" << endl;
-		}
-
-		if (isLogical(ld_fraction)) {
-			cout << "ld_fraction is logical" << endl;
-		}
-
-		if (length(ld_fraction) <= 0) {
-			cout << "ld_fraction is empty" << endl;
-		}
-
-		if (length(ld_fraction) > 1) {
-			cout << "ld_fraction has multiple values" << endl;
-		}
-
-		if (isInteger(ld_fraction)) {
-			c_ld_fraction = (double)INTEGER(ld_fraction)[0];
-		} else {
-			c_ld_fraction = REAL(ld_fraction)[0];
-			if (isnan(c_ld_fraction)) {
-				cout << "c_ld_fraction is not a number" << endl;
-			}
-		}
-
+		c_ld_fraction = validateDouble(ld_fraction, "ld_fraction", false);
 		if ((c_ld_fraction <= 0.0) || (c_ld_fraction > 1.0)) {
 			cout << "c_ld_fraction must be in (0.0, 1.0]" << endl;
 		}
 
 //		Validate pruning_method argument
-		if (isNull(pruning_method)) {
-			cout << "pruning_method is null" << endl;
-		}
-
-		if (!isString(pruning_method)) {
-			cout << "pruning_method is not string" << endl;
-		}
-
-		if (length(pruning_method) <= 0) {
-			cout << "pruning_method is empty" << endl;
-		}
-
-		if (length(pruning_method) > 1) {
-			cout << "pruning_method has multiple values." << endl;
-		}
-
-		if (STRING_ELT(pruning_method, 0) == R_NaString) {
-			cout << "pruning_method is na" << endl;
-		}
-
-		if (STRING_ELT(pruning_method, 0) == R_BlankString) {
-			cout << "pruning_method is blank" << endl;
-		}
-
-		c_pruning_method = CHAR(STRING_ELT(pruning_method, 0));
+		c_pruning_method = validateString(pruning_method, c_pruning_method, false);
 
 //		Validate window argument
-		if (isNull(window)) {
-			cout << "window is null" << endl;
-		}
-
-		if (!isNumeric(window)) {
-			cout << "window is not numeric" << endl;
-		}
-
-		if (isLogical(window)) {
-			cout << "window is logical" << endl;
-		}
-
-		if (length(window) <= 0) {
-			cout << "window is empty" << endl;
-		}
-
-		if (length(window) > 1) {
-			cout << "window has multiple values." << endl;
-		}
-
-		if (isInteger(window)) {
-			c_window_int = INTEGER(window)[0];
-		} else {
-			c_window_double = REAL(window)[0];
-			if (isnan(c_window_double)) {
-				cout << "window is nan" << endl;
-			}
-			c_window_int = (long int)c_window_double;
-		}
-
-		if (c_window_int <= 0) {
+		c_window = validateInteger(window, "window", false);
+		if (c_window <= 0) {
 			cout << "window must be greater than 0" << endl;
 		}
-
 
 		return R_NilValue;
 	}
