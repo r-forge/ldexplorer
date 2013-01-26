9555726fc8f58362635af18c639ca4035f3a0489
dtaliun (Daniel Taliun) <daniel.taliun@eurac.edu>
2013-01-26 14:56:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ldexplorer@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/LDExplorer.cpp
+++ pkg/src/LDExplorer.cpp
@@ -26,63 +26,47 @@ using namespace std;
 
 extern "C" {
 
-	const char* validateString(SEXP value, const char* name, bool optional) {
-		if (isNull(value)) {
-			if (!optional) {
-				error("%s is NULL.", name);
-			} else {
-				return NULL;
-			}
-		}
-
+	const char* validateString(SEXP value, const char* name) {
 		if (!isString(value)) {
-			error("%s is not a string.", name);
+			error("'%s' argument is not a string.", name);
 		}
 
 		if (length(value) <= 0) {
-			error("%s contains no values.", name);
+			error("'%s' argument contains no values.", name);
 		}
 
 		if (length(value) > 1) {
-			error("%s contains multiple values.", name);
+			error("'%s' argument contains multiple values.", name);
 		}
 
 		if (STRING_ELT(value, 0) == R_NaString) {
-			error("%s is NA.", name);
+			error("'%s' argument is NA.", name);
 		}
 
 		if (STRING_ELT(value, 0) == R_BlankString) {
-			error("%s is blank.", name);
+			error("'%s' argument is blank.", name);
 		}
 
 		return CHAR(STRING_ELT(value, 0));
 	}
 
-	double validateDouble(SEXP value, const char* name, bool optional) {
+	double validateDouble(SEXP value, const char* name) {
 		double c_value = numeric_limits<double>::quiet_NaN();
 
-		if (isNull(value)) {
-			if (!optional) {
-				error("%s is NULL.", name);
-			} else {
-				return numeric_limits<double>::quiet_NaN();
-			}
-		}
-
 		if (!isNumeric(value)) {
-			error("%s is not numeric.", name);
+			error("'%s' argument is not numeric.", name);
 		}
 
 		if (isLogical(value)) {
-			error("%s is logical.", name);
+			error("'%s' argument is logical.", name);
 		}
 
 		if (length(value) <= 0) {
-			error("%s contains no values.", name);
+			error("'%s' argument contains no values.", name);
 		}
 
 		if (length(value) > 1) {
-			error("%s contains multiple values.", name);
+			error("'%s' argument contains multiple values.", name);
 		}
 
 		if (isInteger(value)) {
@@ -90,36 +74,28 @@ extern "C" {
 		} else {
 			c_value = REAL(value)[0];
 			if (isnan(c_value)) {
-				error("%s is NA/NaN.", name);
+				error("'%s' argument is NA/NaN.", name);
 			}
 		}
 
 		return c_value;
 	}
 
-	void validateDoubles(SEXP value, const char* name, double* c_value, unsigned int length, bool optional) {
-		if (isNull(value)) {
-			if (!optional) {
-				error("%s is NULL.", name);
-			} else {
-				return;
-			}
-		}
-
+	void validateDoubles(SEXP value, const char* name, double* c_value, unsigned int length) {
 		if (!isNumeric(value)) {
-			error("%s is not numeric.", name);
+			error("'%s' argument is not numeric.", name);
 		}
 
 		if (isLogical(value)) {
-			error("%s is logical.", name);
+			error("'%s' argument is logical.", name);
 		}
 
 		if (length(value) < (long int)length) {
-			error("%s contains less than %u value(s).", name, length);
+			error("'%s' argument contains less than %u value(s).", name, length);
 		}
 
 		if (length(value) > (long int)length) {
-			error("%s contains more than %u value(s).", name, length);
+			error("'%s' argument contains more than %u value(s).", name, length);
 		}
 
 		if (isInteger(value)) {
@@ -130,38 +106,30 @@ extern "C" {
 			for (unsigned int i = 0; i < length; ++i) {
 				c_value[i] = REAL(value)[i];
 				if (isnan(c_value[i])) {
-					error("%s contains NA/NaN value(s).", name);
+					error("'%s' argument contains NA/NaN value(s).", name);
 				}
 			}
 		}
 	}
 
-	long int validateInteger(SEXP value, const char* name, bool optional) {
+	long int validateInteger(SEXP value, const char* name) {
 		double c_value_double = numeric_limits<double>::quiet_NaN();
 		long int c_value_int = numeric_limits<long int>::min();
 
-		if (isNull(value)) {
-			if (!optional) {
-				error("%s is NULL.", name);
-			} else {
-				return numeric_limits<long int>::min();
-			}
-		}
-
 		if (!isNumeric(value)) {
-			error("%s is not numeric.", name);
+			error("'%s' argument is not numeric.", name);
 		}
 
 		if (isLogical(value)) {
-			error("%s is logical.", name);
+			error("'%s' argument is logical.", name);
 		}
 
 		if (length(value) <= 0) {
-			error("%s contains no values.", name);
+			error("'%s' argument contains no values.", name);
 		}
 
 		if (length(value) > 1) {
-			error("%s contains multiple values.", name);
+			error("'%s' argument contains multiple values.", name);
 		}
 
 		if (isInteger(value)) {
@@ -169,7 +137,7 @@ extern "C" {
 		} else {
 			c_value_double = REAL(value)[0];
 			if (isnan(c_value_double)) {
-				error("%s is NA/NaN.", name);
+				error("'%s' argument is NA/NaN.", name);
 			}
 			c_value_int = (long int)c_value_double;
 		}
@@ -177,31 +145,23 @@ extern "C" {
 		return c_value_int;
 	}
 
-	void validateIntegers(SEXP value, const char* name, long int* c_value, unsigned int length, bool optional) {
+	void validateIntegers(SEXP value, const char* name, long int* c_value, unsigned int length) {
 		double c_value_double = numeric_limits<double>::quiet_NaN();
 
-		if (isNull(value)) {
-			if (!optional) {
-				error("%s is NULL.", name);
-			} else {
-				return;
-			}
-		}
-
 		if (!isNumeric(value)) {
-			error("%s is not numeric.", name);
+			error("'%s' argument is not numeric.", name);
 		}
 
 		if (isLogical(value)) {
-			error("%s is logical.", name);
+			error("'%s' argument is logical.", name);
 		}
 
 		if (length(value) < (long int)length) {
-			error("%s contains less than %u value(s).", name, length);
+			error("'%s' argument contains less than %u value(s).", name, length);
 		}
 
 		if (length(value) > (long int)length) {
-			error("%s contains more than %u value(s).", name, length);
+			error("'%s' argument contains more than %u value(s).", name, length);
 		}
 
 		if (isInteger(value)) {
@@ -212,13 +172,12 @@ extern "C" {
 			for (unsigned int i = 0; i < length; ++i) {
 				c_value_double = REAL(value)[i];
 				if (isnan(c_value_double)) {
-					error("%s contains NA/NaN value(s).", name);
+					error("'%s' argument contains NA/NaN value(s).", name);
 				}
 				c_value[i] = (long int)c_value_double;
 			}
 		}
 	}
-
 
 	SEXP mig(SEXP phase_file, SEXP output_file, SEXP file_format, SEXP legend_file,
 			SEXP region, SEXP maf, SEXP ci_method, SEXP ci_precision, SEXP ld_ci, SEXP ehr_ci, SEXP ld_fraction,
@@ -236,80 +195,126 @@ extern "C" {
 		double c_ehr_ci = 0.0;
 		double c_ld_fraction = 0.0;
 		const char* c_pruning_method = NULL;
-		long int c_window = 0;
+		long int c_window = numeric_limits<long int>::max();
 
 //		Validate phase_file argument
-		c_phase_file = validateString(phase_file, "phase_file", false);
+		if (!isNull(phase_file)) {
+			c_phase_file = validateString(phase_file, "phase_file");
+		} else {
+			error("'%s' argument is NULL.", "phase_file");
+		}
 
 //		Validate output_file argument
-		c_output_file = validateString(output_file, "output_file", false);
+		if (!isNull(output_file)) {
+			c_output_file = validateString(output_file, "output_file");
+		} else {
+			error("'%s' argument is NULL.", "output_file");
+		}
 
 //		Validate file_format argument
-		c_file_format = validateString(file_format, "file_format", false);
+		if (!isNull(file_format)) {
+			c_file_format = validateString(file_format, "file_format");
+		} else {
+			error("'%s' argument is NULL.", "file_format");
+		}
 
 //		Validate legend_file argument
-		c_legend_file = validateString(legend_file, "legend_file", true);
+		if (!isNull(legend_file)) {
+			c_legend_file = validateString(legend_file, "legend_file");
+		}
 
 //		Validate region argument
-		validateIntegers(region, "region", c_region, 2u, true);
-		if (c_region[0] < 0) {
-			cout << "start must be positive" << endl;
-		}
-		if (c_region[1] < 0) {
-			cout << "end must be positive" << endl;
-		}
-		if (c_region[0] >= c_region[1]) {
-			cout << "end must be strictly greater than end" << endl;
+		if (!isNull(region)) {
+			validateIntegers(region, "region", c_region, 2u);
+			if (c_region[0] < 0) {
+				error("The region start position, specified in '%s' argument, must be positive.", "region");
+			}
+			if (c_region[1] < 0) {
+				error("The region end position, specified in '%s' argument, must be positive.", "region");
+			}
+			if (c_region[0] >= c_region[1]) {
+				error("The region end position, specified in '%s' argument, must be strictly greater than the region start position.", "region");
+			}
+		} else {
 		}
 
 //		Validate maf argument
-		c_maf = validateDouble(maf, "maf", false);
-		if ((c_maf < 0.0) || (c_maf > 0.5)) {
-			cout << "c_maf must be in [0, 0.5]" << endl;
+		if (!isNull(maf)) {
+			c_maf = validateDouble(maf, "maf");
+			if ((c_maf < 0.0) || (c_maf > 0.5)) {
+				error("The minor allele frequency, specified in '%s' argument, must be in [0, 0.5] interval.", "maf");
+			}
+		} else {
+			error("'%s' argument is NULL.", "maf");
 		}
 
 //		Validate ci_method argument
-		c_ci_method = validateString(ci_method, "ci_method", false);
+		if (!isNull(ci_method)) {
+			c_ci_method = validateString(ci_method, "ci_method");
+		} else {
+			error("'%s' argument is NULL.", "ci_method");
+		}
 
 //		Validate ci_precision argument
-		c_ci_precision = validateInteger(ci_precision, "ci_precision", false);
-		if (c_ci_precision <= 0) {
-			cout << "ci_precision must be greater than 0" << endl;
+		if (!isNull(ci_precision)) {
+			c_ci_precision = validateInteger(ci_precision, "ci_precision");
+			if (c_ci_precision <= 0) {
+				error("The number of likelihood estimation points to compute confidence interval, specified in '%s' argument, must be strictly greater then 0.", "ci_precision");
+			}
+		} else {
+			error("'%s' argument is NULL.", "ci_precision");
 		}
 
 //		Validate ld_ci argument
-		validateDoubles(ld_ci, "ld_ci", c_ld_ci, 2u, false);
-		if ((c_ld_ci[0] < 0.0) || (c_ld_ci[0] > 1.0)) {
-			cout << "CL must be in [0.0, 1.0]" << endl;
-		}
-
-		if ((c_ld_ci[1] < 0.0) || (c_ld_ci[1] > 1.0)) {
-			cout << "CU must be in [0.0, 1.0]" << endl;
-		}
-
-		if (c_ld_ci[0] >= c_ld_ci[1]) {
-			cout << "CL must be greater than CU" << endl;
+		if (!isNull(ld_ci)) {
+			validateDoubles(ld_ci, "ld_ci", c_ld_ci, 2u);
+			if ((c_ld_ci[0] < 0.0) || (c_ld_ci[0] > 1.0)) {
+				error("The lower bound of confidence interval, specified in '%s' argument, must be in [0, 1] interval.", "ld_ci");
+			}
+			if ((c_ld_ci[1] < 0.0) || (c_ld_ci[1] > 1.0)) {
+				error("The upper bound of confidence interval, specified in '%s' argument, must be in [0, 1] interval.", "ld_ci");
+			}
+			if (c_ld_ci[0] >= c_ld_ci[1]) {
+				error("The upper bound of confidence interval, specified in '%s' argument, must be greater than the lower bound.", "ld_ci");
+			}
+		} else {
+			error("'%s' argument is NULL.", "ld_ci");
 		}
 
 //		Validate ehr_ci argument
-		c_ehr_ci = validateDouble(ehr_ci, "ehr_ci", false);
-		if ((c_ehr_ci < 0.0) || (c_ehr_ci > 1.0)) {
-			cout << "c_ehr_ci must be in [0.0, 1.0]" << endl;
+		if (!isNull(ehr_ci)) {
+			c_ehr_ci = validateDouble(ehr_ci, "ehr_ci");
+			if ((c_ehr_ci < 0.0) || (c_ehr_ci > 1.0)) {
+				error("The upper bound of confidence interval, specified in '%s' argument, must be in [0, 1] interval.", "ehr_ci");
+			}
+		} else {
+			error("'%s' argument is NULL.", "ehr_ci");
 		}
 
 //		Validate ld_fraction argument
-		c_ld_fraction = validateDouble(ld_fraction, "ld_fraction", false);
-		if ((c_ld_fraction <= 0.0) || (c_ld_fraction > 1.0)) {
-			cout << "c_ld_fraction must be in (0.0, 1.0]" << endl;
+		if (!isNull(ld_fraction)) {
+			c_ld_fraction = validateDouble(ld_fraction, "ld_fraction");
+			if ((c_ld_fraction <= 0.0) || (c_ld_fraction > 1.0)) {
+				error("The fraction of strong LD SNP pairs within a haplotype block, specified in '%s' argument, must be in (0.0, 1.0] interval.", "ld_fraction");
+			}
+		} else {
+			error("'%s' argument is NULL.", "ld_fraction");
 		}
 
 //		Validate pruning_method argument
-		c_pruning_method = validateString(pruning_method, c_pruning_method, false);
+		if (!isNull(pruning_method)) {
+			c_pruning_method = validateString(pruning_method, "pruning_method");
+		} else {
+			error("'%s' argument is NULL.", "pruning_method");
+		}
 
 //		Validate window argument
-		c_window = validateInteger(window, "window", true);
-		if (c_window <= 0) {
-			cout << "window must be greater than 0" << endl;
+		if (!isNull(window)) {
+			c_window = validateInteger(window, "window");
+			if (c_window <= 0) {
+				error("The window size, specified in '%s' argument, must be strictly greater than 0.", "window");
+			}
+		} else {
 		}
 
 		return R_NilValue;
