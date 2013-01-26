81e0e8ca04b4d0af05edd2ba126bd193fa098ae2
dtaliun (Daniel Taliun) <daniel.taliun@eurac.edu>
2013-01-26 15:35:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ldexplorer@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/exception/Exception.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright © 2013 Daniel Taliun and Cristian Pattaro. All rights reserved.
+ *
+ * This file is part of LDExplorer.
+ *
+ * LDExplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LDExplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LDExplorer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "include/Exception.h"
+
+const int Exception::MESSAGE_BUFFER_SIZE = 8000;
+
+Exception::Exception(const char* source, int source_line, const char* text_message, ... ): exception() {
+	va_list arguments;
+
+	va_start(arguments, text_message);
+	add_message(source, source_line, text_message, arguments);
+	va_end(arguments);
+}
+
+Exception::~Exception() throw() {
+	list<message*>::iterator trace_it;
+
+	trace_it = trace.begin();
+	while (trace_it != trace.end()) {
+		if ((*trace_it)->text != NULL) {
+			free((*trace_it)->text);
+			(*trace_it)->text = NULL;
+			(*trace_it)->source = NULL;
+		}
+		delete *trace_it;
+
+		trace_it++;
+	}
+
+	trace.clear();
+}
+
+void Exception::format_message_text(char** text, const char* text_template, va_list arguments) {
+	if (*text != NULL) {
+		free(*text);
+		*text = NULL;
+	}
+
+	*text = (char*)malloc(MESSAGE_BUFFER_SIZE * sizeof(char));
+	if (*text != NULL) {
+		if (vsprintf(*text, text_template, arguments) < 0) {
+			free(*text);
+			*text = NULL;
+		}
+	}
+}
+
+void Exception::add_message(const char* source, int source_line, const char* text_message, va_list arguments) {
+	message* msg = new message();
+
+	format_message_text(&(msg->text), text_message, arguments);
+	msg->source = source;
+	msg->source_line = source_line;
+
+	trace.push_front(msg);
+}
+
+void Exception::add_message(const char* source, int source_line, const char* text_message, ... ) {
+	va_list arguments;
+
+	va_start(arguments, text_message);
+	add_message(source, source_line, text_message, arguments);
+	va_end(arguments);
+}
+
+const char* Exception::what() const throw() {
+	stringstream string_stream;
+	list<message*>::const_iterator trace_it;
+
+	string_stream << setfill(' ');
+	trace_it = trace.begin();
+	while (trace_it != trace.end()) {
+		if ((*trace_it)->source != NULL) {
+			string_stream << (*trace_it)->source << " ";
+		}
+
+		string_stream << "(" << (*trace_it)->source_line << ") : ";
+
+		if ((*trace_it)->text != NULL) {
+			string_stream << (*trace_it)->text;
+		} else {
+			string_stream << "Exception.";
+		}
+
+		string_stream << endl;
+		trace_it++;
+	}
+
+	return string_stream.str().c_str();
+}
---
+++ pkg/src/exception/include/Exception.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2013 Daniel Taliun and Cristian Pattaro. All rights reserved.
+ *
+ * This file is part of LDExplorer.
+ *
+ * LDExplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LDExplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LDExplorer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EXCEPTION_H_
+#define EXCEPTION_H_
+
+#include <exception>
+#include <limits>
+#include <list>
+#include <cstdarg>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+class Exception: public exception {
+private:
+	static const int MESSAGE_BUFFER_SIZE;
+
+	struct message {
+		char* text;
+		const char* source;
+		int source_line;
+		message(): text(NULL), source(NULL), source_line(numeric_limits<int>::min()) {};
+	};
+
+	list<message*> trace;
+
+	void format_message_text(char** text, const char* text_template, va_list arguments);
+	void add_message(const char* source, int source_line, const char* text_message, va_list arguments);
+
+public:
+	Exception(const char* source, int source_line, const char* text_message, ... );
+	virtual ~Exception() throw();
+	void add_message(const char* source, int source_line, const char* text_message, ... );
+	virtual const char* what() const throw();
+};
+
+#endif
