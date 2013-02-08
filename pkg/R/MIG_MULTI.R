f811465366c18bc5d070a0f4e721c1adfa890c71
dtaliun (Daniel Taliun) <daniel.taliun@eurac.edu>
2013-02-08 13:31:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ldexplorer@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/MIG_MULTI.R
@@ -0,0 +1,28 @@
+#
+# Copyright © 2013 Daniel Taliun, Johann Gamper and Cristian Pattaro. All rights reserved.
+#
+# This file is part of LDExplorer.
+#
+# LDExplorer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LDExplorer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LDExplorer.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+mig_multi <- function(phase_files, output_files, phase_file_format = "VCF", map_files = NULL, maf = 0.0, ci_method = "WP", l_density = 100, ld_ci = c(0.7, 0.98), ehr_ci = 0.9, ld_fraction = 0.95, pruning_method = "MIG++", windows = NULL) {
+	if (missing(phase_files)) {
+		stop("The 'phase_file' argument is missing.");
+	}
+	
+	if (missing(output_files)) {
+		stop("The 'output_file' argument is missing.");
+	}
+}
