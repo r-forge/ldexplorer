ec6e0cf1ece4bb17c746f849b684e0c87f92447a
dtaliun (Daniel Taliun) <daniel.taliun@eurac.edu>
2013-02-09 12:56:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ldexplorer@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MIG_MULTI.R
+++ pkg/R/MIG_MULTI.R
@@ -17,7 +17,7 @@
 # along with LDExplorer.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-mig_multi <- function(phase_files, output_files, phase_file_format = "VCF", map_files = NULL, maf = 0.0, ci_method = "WP", l_density = 100, ld_ci = c(0.7, 0.98), ehr_ci = 0.9, ld_fraction = 0.95, pruning_method = "MIG++", windows = NULL) {
+mig_multi <- function(phase_files, output_files, processes = 1, phase_file_format = "VCF", map_files = NULL, maf = 0.0, ci_method = "WP", l_density = 100, ld_ci = c(0.7, 0.98), ehr_ci = 0.9, ld_fraction = 0.95, pruning_method = "MIG++", windows = NULL) {
 	if (missing(phase_files)) {
 		stop("The 'phase_files' argument is missing.");
 	}
@@ -68,6 +68,23 @@ mig_multi <- function(phase_files, output_files, phase_file_format = "VCF", map_
 		}
 	}
 	
+	if (is.numeric(processes)) {
+		if (abs(processes - round(processes)) < .Machine$double.eps ^ 0.5) {
+			processes <- as.integer(processes)
+			if (is.na(processes)) {
+				stop("Error while casting number of processes argument to integer.")
+			} else if (processes < 1) {
+				stop("The number of processes must be greater than 0.")	
+			}
+		} else {
+			stop("The number of processes argument must be integer.")
+		}
+	} else {
+		stop("The number of processes argument must be numeric.")
+	}
+	
+	args_per_cluster <- list()
+	
 	for (i in 1:length(phase_files)) {
 		phase_file <- phase_files[i]
 		output_file <- output_files[i]
@@ -83,7 +100,49 @@ mig_multi <- function(phase_files, output_files, phase_file_format = "VCF", map_
 		} else {
 			window <- NULL
 		}
-			
-		mig(phase_file, output_file, phase_file_format, map_file, NULL, maf, ci_method, l_density, ld_ci, ehr_ci, ld_fraction, pruning_method, window)
+		
+		args <- list(phase_file = phase_file, output_file = output_file, phase_file_format = phase_file_format, map_file = map_file, region = NULL, maf = maf, ci_method = ci_method, l_density = l_density, ld_ci = ld_ci, ehr_ci = ehr_ci, ld_fraction = ld_fraction, pruning_method = pruning_method, window = window)
+		
+		args_per_cluster <- c(args_per_cluster, list(args))
+	}
+	
+	if (processes == 1) {
+		for (i in 1:length(args_per_cluster)) {
+			x <- args_per_cluster[[i]]
+			mig(x$phase_file, x$output_file, x$phase_file_format, x$map_file, x$region, x$maf, x$ci_method, x$l_density, x$ld_ci, x$ehr_ci, x$ld_fraction, x$pruning_method, x$window)
+		}
+	} else {
+		cat("Initializing cluster... ")
+		start_time <- proc.time()
+		
+		require(parallel, quietly = TRUE)
+	
+		clusters <- makeCluster(rep("localhost", 2), type="SOCK")
+		assign("ldexplorer_package_path", dirname(.path.package("LDExplorer")), envir = .GlobalEnv)
+		clusterExport(clusters, "ldexplorer_package_path")
+		clusterEvalQ(clusters, .libPaths(union(ldexplorer_package_path, .libPaths())))
+		clusterEvalQ(clusters, library(LDExplorer))
+		
+		elapsed_time <- proc.time() - start_time
+		cat("Done (", elapsed_time[3], " sec).\n", sep="")
+		
+		cat("Processing... ")
+		start_time <- proc.time()
+		
+		cluster_result <- clusterApply(clusters, args_per_cluster, function(x) {
+					sink(paste(x$output_file, ".log", sep=""), split=F)
+					mig(x$phase_file, x$output_file, x$phase_file_format, x$map_file, x$region, x$maf, x$ci_method, x$l_density, x$ld_ci, x$ehr_ci, x$ld_fraction, x$pruning_method, x$window)
+				})
+		
+		elapsed_time <- proc.time() - start_time
+		cat("Done (", elapsed_time[3], " sec).\n", sep="")
+		
+		cat("Stopping cluster... ")
+		start_time <- proc.time()
+		
+		stopCluster(clusters)
+		
+		elapsed_time <- proc.time() - start_time
+		cat("Done (", elapsed_time[3], " sec).\n", sep="")
 	}
 }
